a0d8d302f8ed892fa82c604935200083c762f482
Julian <julianrosenthal@yahoo.com>
2021-03-02 17:15:14 -0700

    commit tree 21488b66196b796c8938542d0c7cc3726ccc005d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+
+let map = (arr, fn) => arr.map(fn);
+let reduce = function(arr, fn, start) {
+  let r = (!!start) ? start : arr[0]
+  let i = (!!start) ? 0 : 1
+
+  for (; i < arr.length; i++) {
+    r = fn(arr[i], r)
+  }
+
+  return r;
+}
